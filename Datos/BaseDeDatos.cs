b305cb113a6cafd5e85d030dbe848ff5d6690afc
nmanoche <nelson.manoche@gmail.com>
2022-05-08 20:30:59 -0300

    Se agrega TestProductos OK

--- Datos/BaseDeDatos.cs
+++ Datos/BaseDeDatos.cs
@@ -9,7 +9,24 @@ namespace CarritoDeCompras.Datos
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TestProducto>()
+                .HasOne<Categoria>(s => s.Categoria)
+                .WithMany(g => g.TestProductos)
+                .HasForeignKey(s => s.IdCategoria);
+
+            modelBuilder.Entity<TestProducto>()
+                .HasOne<Marca  >(s => s.Marca)
+                .WithMany(g => g.TestProductos)
+                .HasForeignKey(s => s.IdMarca);
+        }
+
         public DbSet<Usuario> Usuarios { get; set; }
         public DbSet<Producto> Productos { get; set; }
+        public DbSet<CarritoDeCompras.Models.Categoria> Categorias { get; set; }
+        public DbSet<CarritoDeCompras.Models.Marca> Marcas { get; set; }
+        public DbSet<CarritoDeCompras.Models.TestProducto> TestProductos { get; set; }
     }
 }
